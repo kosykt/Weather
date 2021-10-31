d3a038c8a082d427da4acf080cc4e1e102c95cab
konstantin <kosval09@gmail.com>
2021-10-31 23:35:09 +0900

    les11 не замещающиеся уведомления firebase

--- app/src/main/java/ru/kostry/weather/cloudmessage/MyFirebaseMessagingService.kt
+++ app/src/main/java/ru/kostry/weather/cloudmessage/MyFirebaseMessagingService.kt
@@ -1,23 +1,20 @@
 package ru.kostry.weather.cloudmessage
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import ru.kostry.weather.R
 
 //идентификатор канала
-private const val CHANNEL_ID = "CHANNEL_MAIN"
+public const val CHANNEL_ID = "CHANNEL_MAIN"
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    //
-    private val notificationId = 10
+    //var изменяя ИД помогает не заменять старые сообщения на новые, а показывать их оба
+    private var notificationId = 10
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         //получаем тайтл сообщения
@@ -31,7 +28,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     //показать сообщение//нотификация между программами
     private fun showNotification(title: String, message: String) {
         //билдер создает канал
-        val notificationBuilder = NotificationCompat.Builder(applicationContext, CHANNEL_ID).apply{
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, CHANNEL_ID).apply {
             //установить иконку, тайтл, текст
             setSmallIcon(R.drawable.ic_kotlin_logo)
             setContentTitle(title)
@@ -41,27 +38,14 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
 
         //получть NotificationManager из Context.NOTIFICATION_SERVICE
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        //если версия новая, то открыть канал
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            createNotificationChannel(notificationManager)
-        }
+        //изменить счетчик ИД
+        notificationId++
 
         //построить сообщение
         notificationManager.notify(notificationId, notificationBuilder.build())
-    }
-
-    //создать новый канал
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(notificationManager: NotificationManager) {
-        val channelName = "Channel name"
-        val descriptionText = "Channel description"
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
-            description = descriptionText
-        }
-        notificationManager.createNotificationChannel(channel)
     }
 
     //сохранить и отправить на сервер
--- app/src/main/java/ru/kostry/weather/view/MainActivity.kt
+++ app/src/main/java/ru/kostry/weather/view/MainActivity.kt
@@ -1,10 +1,19 @@
 package ru.kostry.weather.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.kostry.weather.R
+import ru.kostry.weather.cloudmessage.CHANNEL_ID
 import ru.kostry.weather.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +29,38 @@ class MainActivity : AppCompatActivity() {
                 .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
+
+        //если версия новая, то открыть канал
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //получть NotificationManager из Context.NOTIFICATION_SERVICE
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            createNotificationChannel(notificationManager)
+        }
+
+        //получить токен и отправить обратно les11 01.50.00
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FIREBASEMSG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+            Log.d("FIREBASEMSG", token!!)
+        })
+
+    }
+
+    //создать новый канал
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channelName = "Channel name"
+        val descriptionText = "Channel description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
     }
 
     //создаем меню
